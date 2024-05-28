a25e7390e160e16cc9df0371dc5bf628d9f719a2
EasonLin0716 <uu9924079@gmail.com>
2024-05-28 13:59:40 +0800

    chore: add typescript build tools to project

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,20 @@ const config = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
     ],
   },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
 };
 
 module.exports = config;
