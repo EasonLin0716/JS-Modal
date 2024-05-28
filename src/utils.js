e7ad1c055f918783988e53638567830339fed2d3
Eason Lin <eason_lin@addcn.com>
2024-05-28 13:48:24 +0800

    style: format codes with tools

--- src/utils.js
+++ src/utils.js
@@ -6,33 +6,33 @@
  * @param {number} duration 淡入的毫秒數，預設為400，若設為0則跳過淡入動畫直接呈現
  * @param {function} callback 淡入後呼叫的函式
  */
- export const fadeIn = function (el, display="inline-block", duration=400, callback) {
-  if (!el) {
-    throw new Error('missing el argument');
-  };
+export const fadeIn = function (el, display = "inline-block", duration = 400, callback) {
+    if (!el) {
+        throw new Error('missing el argument');
+    };
 
-  el.style.opacity = el.style.opacity || 0;
-  el.style.display = display;
-  el.style.visibility = "visible";
+    el.style.opacity = el.style.opacity || 0;
+    el.style.display = display;
+    el.style.visibility = "visible";
 
-  if(duration) {
-      let opacity = parseFloat(el.style.opacity) || 0;
-      const timer = setInterval( function() {
-      opacity += 20 / duration;
-      if( opacity >= 1 ) {
-          clearInterval(timer);
-          opacity = 1;
-          if (typeof callback === 'function') {
-              callback();
-          }
-      }
-      el.style.opacity = opacity;
-      }, 20 );
-  }
-  else {
-      el.style.opacity = 1;
-  }
-}; 
+    if (duration) {
+        let opacity = parseFloat(el.style.opacity) || 0;
+        const timer = setInterval(function () {
+            opacity += 20 / duration;
+            if (opacity >= 1) {
+                clearInterval(timer);
+                opacity = 1;
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }
+            el.style.opacity = opacity;
+        }, 20);
+    }
+    else {
+        el.style.opacity = 1;
+    }
+};
 
 /**
 * 淡出功能
@@ -41,29 +41,29 @@
 * @param {number} duration 淡出的毫秒數，預設為400，若設為0則直接跳過淡出動畫直接消失
 * @param {function} callback 淡出後呼叫的函式
 */
-export const fadeOut = function(el, duration=400, callback) {
+export const fadeOut = function (el, duration = 400, callback) {
     if (!el) {
         throw new Error('missing el argument');
     };
 
-  if(duration) {
-      let opacity = 1;
-      const timer = setInterval( function() {
-      opacity -= 20 / duration;
-      if(opacity <= 0) {
-          clearInterval(timer);
-          opacity = 0;
-          el.style.display = "none";
-          el.style.visibility = "hidden";
-          if (typeof callback === 'function') {
-              callback();
-          }
-      }
-      el.style.opacity = opacity;
-      }, 20);
-  } else {
-      el.style.opacity = 0;
-      el.style.display = "none";
-      el.style.visibility = "hidden";
-  }
+    if (duration) {
+        let opacity = 1;
+        const timer = setInterval(function () {
+            opacity -= 20 / duration;
+            if (opacity <= 0) {
+                clearInterval(timer);
+                opacity = 0;
+                el.style.display = "none";
+                el.style.visibility = "hidden";
+                if (typeof callback === 'function') {
+                    callback();
+                }
+            }
+            el.style.opacity = opacity;
+        }, 20);
+    } else {
+        el.style.opacity = 0;
+        el.style.display = "none";
+        el.style.visibility = "hidden";
+    }
 };
